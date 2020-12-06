1191743d7bf3f835f29be321614de6cc62d18249
John Loucks <johnloucks@gmail.com>
2020-12-05 19:03:23 -0600

    testing functions, circles but no colors

--- logic.js
+++ logic.js
@@ -17,9 +17,8 @@ function createFeatures(earthquakeData) {
   
     var earthquakes = L.geoJSON(earthquakeData, {
     // Define a function we want to run once for each feature in the features array
-    // Give each feature a popup describing the place and time of the earthquake
-   onEachFeature : function (feature, mapObject) {
-  
+    // Give each feature a popup and create circle marker for each quake
+   onEachFeature : function (feature, mapObject) {  
     mapObject.bindPopup("<h3>" + feature.properties.place +
         "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + 
         "<p> Magnitude: " +  feature.properties.mag + "</p>")
@@ -37,3 +36,37 @@ function createFeatures(earthquakeData) {
     createMap(earthquakes); 
 };
 
+function createMap(earthquakes) {
+
+    // Define map layers
+    var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 512,
+      maxZoom: 18,
+      zoomOffset: -1,
+      id: "mapbox/streets-v11",
+      accessToken: API_KEY
+  });
+
+
+    var baseMaps = {
+        "Satellite Map": satellitemap,
+        
+    };
+
+    var overlayMaps = {
+        "Earthquakes": earthquakes,
+        
+    };
+
+    var myMap = L.map("mapid", {
+        center: [37.09,-95.75],
+        zoom: 4,
+        layers: [satellitemap, earthquakes]
+    });
+
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+      }).addTo(myMap);
+
+}
