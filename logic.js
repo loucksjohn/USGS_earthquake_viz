11b60a8924a21b624ced2964cd4bb7d8667cfe8f
John Loucks <johnloucks@gmail.com>
2020-12-05 20:58:18 -0600

    working on colors and legend

--- logic.js
+++ logic.js
@@ -3,7 +3,23 @@ function markerSize(mag) {
 }
 
 
-function markerColor(depth) {}
+function markerColor(depth) {
+    if (depth <= 10) {
+        return "#7CFC00";
+    } else if (depth <= 30) {
+        return "#ADFF2F";
+    } else if (depth <= 50) {
+        return "#FFD700";
+    } else if (depth <= 70) {
+        return "#FFA500";
+    } else if (depth <= 90) {
+        return "#FF8C00";
+    } else {
+        return "#FF0000";
+    };
+
+
+}
 
 
 var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
@@ -19,13 +35,14 @@ function createFeatures(earthquakeData) {
     // Define a function we want to run once for each feature in the features array
     // Give each feature a popup and create circle marker for each quake
    onEachFeature : function (feature, mapObject) {  
-    mapObject.bindPopup("<h3>" + feature.properties.place +
+    mapObject.bindPopup("<h3>" + feature.properties.title +
         "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + 
-        "<p> Magnitude: " +  feature.properties.mag + "</p>")
+        "<p> Magnitude: " +  feature.properties.mag + "</p>" +
+        "<p> Depth: " +  feature.geometry.coordinates[2] + "</p>")
       },     pointToLayer: function (feature, latlng) {
         return new L.circle(latlng,
           {radius: markerSize(feature.properties.mag),
-          fillColor: markerColor(feature.properties.mag),
+          fillColor: markerColor(feature.geometry.coordinates[2]),
           fillOpacity: .9,
           stroke: true,
           color: "gray",
@@ -59,7 +76,9 @@ function createMap(earthquakes) {
 
     var dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        tileSize: 512,
         maxZoom: 18,
+        zoomOffset: -1,
         id: "dark-v10",
         accessToken: API_KEY      
     });
@@ -77,13 +96,42 @@ function createMap(earthquakes) {
     };
 
     var myMap = L.map("mapid", {
-        center: [37.09,-95.75],
+        center: [40.167, -100.167],
         zoom: 4,
         layers: [satellitemap, earthquakes]
     });
 
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
-      }).addTo(myMap);
+    }).addTo(myMap);
+
+    function getColor(d) {
+        return d> 90 ? "#FF0000":
+        d>70?"#FF8C00":
+        d>50?"#FFA500":
+        d>30?"#FFD700":
+        d>10? "#ADFF2F":
+        d? "#7CFC00":
+        "FFEDA0"
+    };
+
+    var legend = L.control({ position: "bottomright" });
+    legend.onAdd = function() {
+        var div = L.DomUtil.create("div", "info legend");
+        depths= [0,10,30,50,70,90],
+        labels=[];
+    //Loop through depth values scale in legend
+    for (var i = 0; i < depths.length; i++) {
+    div.innerHTML += 
+    '<i style="background:' + getColor(depths[i] + 1) + '"></i> ' 
+    + depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+');
+    };
+    return div;
+    
+    };
+    
+      legend.addTo(myMap);
+
+    
 
 }
